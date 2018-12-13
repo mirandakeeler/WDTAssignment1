6d1125336789d6578c0a1852d8e77fc76d4950e5
Miranda <keeler.miranda@gmail.com>
2018-12-13 14:41:02 +0100

    updated JavaScript and game.html/gamestylesheet.css

--- app.js
+++ app.js
@@ -1,49 +1,24 @@
-var gameStatModule = {function () {
+var gameBoardModule = (function() {
+	var pieces = {
+		KING: { name: 'King', white: '&#9812', black: '&#9818;' },
+		QUEEN: { name: 'Queen', white: '&#9813', black: '&#9819' },
+		ROOK: { name: 'Rook', white: '&#9814', black: '&#9820' },
+		BISHOP: { name: 'Bishop', white: '&#9815', black: '&#9821' },
+		KNIGHT: { name: 'Knight', white: '&#9816', black: '&#9822' },
+		PAWN: { name: 'Pawn', white: '&#9817', black: '&#9823' }
+	};
 
-    // declare pieces variables
-        var knight = {piece: "knight", white: "&#9815;", black: "&#9822;"}; 
-        var king = {piece: "king", white: "&#9812;", black: "&#9818;"}; 
-        var queen = {piece: "queen", white: "&#9813;", black: "&#9819;"};  
-        var pawn = {piece: "pawn", white: "&#9817;", black: "&#9823;"};  
-        var rook = {piece: "rook", white: "&#9814;", black: "&#9820;"}; 
-        var bishop = {piece: "bishop", white: "&#9815;", black: "&#9821;"}; 
-}}; 
+	var players = {
+		BLACK: 'black',
+		WHITE: 'white'
+	};
 
-// create the chessboard with 2 loops
+	var states = {
+		READY: 1,
+		PIECE_SELECTED: 2
+	};
 
-
-var chessBoard = function(){
-var size = 8;
-
-for (var lineCounter = 1; lineCounter < size; lineCounter++) { 
-
-    if (lineCounter % 2 === 0) { //if lineCounter is an even number
-        for (var charCounter = 1; charCounter < size; charCounter++) {
-            var evenOdd = (charCounter%2 === 0);
-            switch (evenOdd) {
-                case true:
-                    chessBoard += "#";
-                    break;
-                case false:
-                    chessBoard += " ";
-                    break;
-                }
-            }                   
-        }
-    else { //if lineCounter is an odd number
-        for (var charCounter = 1; charCounter < size; charCounter++) {
-            var evenOdd = (charCounter % 2 === 0);
-            switch (evenOdd) {
-                case true:
-                    chessBoard += " ";
-                    break;
-                case false:
-                    chessBoard += "#";
-                    break;
-            }
-        }                       
-    }   
-    chessBoard += "\n";
-}
-console.log(chessBoard);
-};
+	var takenPieces = {
+		white: [ ],
+		black: [ ]
+	};
